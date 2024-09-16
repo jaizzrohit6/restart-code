b3bd06400c6f21d3acc02eb47912f1b247b4f30a
jaizzrohit6 <rohit1952425@gmail.com>
2024-09-16 23:40:01 +0530

    restart2 started

--- restart-01/restart_001.js
+++ restart-01/restart_001.js
@@ -5,5 +5,29 @@ console.log("Hello World")
 //rohit // tihro
 
 function anagram(str1,str2){
-    
+    str1 = str1.toLowerCase()
+    str2= str2.toLowerCase()
+    if (str1.length !== str2.length) return false;
+    let frequency1 = {}
+    let frequency2 = {}
+    for (let val of str1){
+        frequency1[val] = (frequency1[val] || 0 ) + 1
+    }
+    for (let val of str2){
+        frequency2[val] = (frequency2[val] || 0 ) + 1
+    }
+    const isAnagram =
+    Object.keys(frequency1).every(
+      (key) => frequency2.hasOwnProperty(key) && frequency1[key] === frequency2[key]
+    ) && 
+    Object.keys(frequency2).every(
+      (key) => frequency1.hasOwnProperty(key)
+    );
+
+  return isAnagram;
 }
+let str1 = "rohit"
+let str2= "thiRo"
+console.log(anagram(str1,str2))
+
+
